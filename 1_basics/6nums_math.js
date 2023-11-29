fac2345d90d53af5cd7fe27bce30b97d900f3933
HimanshuWalani <142258734+HimanshuWalani@users.noreply.github.com>
2023-11-29 13:36:28 +0000

    num_math

---
+++ 1_basics/6nums_math.js
@@ -0,0 +1,26 @@
+const num=500;
+// console.log(num);
+// const num1=new Number(100.5455)
+// console.log(num1);
+
+// console.log(num1.toString().length);
+// console.log(num1.toFixed(2));
+// console.log(num1.toPrecision(4));
+// console.log(num1.toPrecision(3));
+
+// const num2=1000000
+// console.log(num2.toLocaleString('en-IN'))//for coma separated values
+//you can also find other function in console Number. and other function can be displayed
+/*****************MATH*********** */
+// console.log(Math.max(4,5,6,7,0));
+// console.log(Math.min(4,5,6,7,0));
+// console.log(`floor of 4.500 is ${Math.floor(4.500)}`);
+// console.log(`ceil of 4.500 is ${Math.ceil(4.500)}`);
+// console.log(Math.abs(-4));
+//round
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
